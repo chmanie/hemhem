e4456a813c31f85b38f06df42331fc769cb04da4
Christian Maniewski <chmanie@fastmail.com>
2019-04-21 22:29:45 +0800

    Add Frankensteins postfix conf

---
+++ conf/init.sql
@@ -0,0 +1,50 @@
+# https://thomas-leister.de/en/mailserver-debian-stretch/
+
+create database vmail
+    CHARACTER SET 'utf8';
+
+grant select on vmail.* to 'vmail'@'%' identified by 'vmaildbpass';
+
+use vmail;
+
+CREATE TABLE `domains` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `domain` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`domain`)
+);
+
+CREATE TABLE `accounts` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `email` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `quota` int unsigned DEFAULT '0',
+    `enabled` boolean DEFAULT '0',
+    `sendonly` boolean DEFAULT '0',
+    PRIMARY KEY (id),
+    UNIQUE KEY (`email`)
+);
+
+CREATE TABLE `aliases` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `source` varchar(255) NOT NULL,
+    `destination` varchar(255) NOT NULL,
+    `enabled` boolean DEFAULT '0',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`source`, `destination`)
+);
+
+CREATE TABLE `tlspolicies` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `domain` varchar(255) NOT NULL,
+    `policy` enum('none', 'may', 'encrypt', 'dane', 'dane-only', 'fingerprint', 'verify', 'secure') NOT NULL,
+    `params` varchar(255),
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`domain`)
+);
+
+insert into domains (domain) values ('konfetti.xyz');
+
+insert into accounts (email, password, quota, enabled, sendonly) values ('chris@konfetti.xyz', encrypt('password', CONCAT('$5$', MD5(RAND()))), 2048, true, false);
+
+insert into aliases (source, destination, enabled) values ('alias@konfetti.xyz', 'chris@konfetti.xyz', true);
